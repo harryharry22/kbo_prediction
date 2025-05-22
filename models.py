8dc791e9310dcb42ae0e1c6446aaa8382520dae5
harry <127291680+harryharry22@users.noreply.github.com>
2025-05-22 23:31:54 +0900

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from database import db  # 변경된 임포트
+from database import db
 
 class WinProbability(db.Model):
     __tablename__ = 'win_probability'
@@ -27,3 +27,50 @@ class RankingPredict(db.Model):
         db.UniqueConstraint('team', 'created_date', name='unique_team_date'),
         {'extend_existing': True}
     )
+
+class HitterRecord(db.Model):
+    __tablename__ = 'hitter_record'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    선수명 = db.Column(db.String(50))
+    팀명 = db.Column(db.String(20))
+    연도 = db.Column(db.Integer)
+    AVG = db.Column(db.Float)
+    G = db.Column(db.Integer)
+    PA = db.Column(db.Integer)
+    AB = db.Column(db.Integer)
+    R = db.Column(db.Integer)
+    H = db.Column(db.Integer)
+    _2B = db.Column('2B', db.Integer)  # 숫자로 시작하는 컬럼명 처리
+    _3B = db.Column('3B', db.Integer)
+    HR = db.Column(db.Integer)
+    TB = db.Column(db.Integer)
+    RBI = db.Column(db.Integer)
+    SAC = db.Column(db.Integer)
+    SF = db.Column(db.Integer)
+    OPS_predict = db.Column(db.Float)
+    created_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+class PitcherRecord(db.Model):
+    __tablename__ = 'pitcher_record'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    선수명 = db.Column(db.String(50))
+    팀명 = db.Column(db.String(20))
+    연도 = db.Column(db.Integer)
+    ERA = db.Column(db.Float)
+    G = db.Column(db.Integer)
+    W = db.Column(db.Integer)
+    L = db.Column(db.Integer)
+    SV = db.Column(db.Integer)
+    HLD = db.Column(db.Integer)
+    WPCT = db.Column(db.Float)
+    IP = db.Column(db.Float)
+    H = db.Column(db.Integer)
+    HR = db.Column(db.Integer)
+    BB = db.Column(db.Integer)
+    HBP = db.Column(db.Integer)
+    SO = db.Column(db.Integer)
+    R = db.Column(db.Integer)
+    ER = db.Column(db.Integer)
+    WHIP = db.Column(db.Float)
+    WHIP_predict = db.Column(db.Float)
+    created_date = db.Column(db.DateTime, default=datetime.utcnow)
